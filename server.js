ffb43895e27f5da5f8cb87c6c51f7e4e47c4b6b4
Rahul Sethi <ramideas@users.noreply.github.com>
2022-07-15 12:41:36 +0100

    refactor worker to use module format
    instead of addEventListener

--- server.js
+++ server.js
@@ -1,4 +1,33 @@
-import { createEventHandler } from '@remix-run/cloudflare-workers';
+import { createEventHandler, createRequestHandler, handleAsset } from '@remix-run/cloudflare-workers';
 import * as build from '@remix-run/dev/server-build';
 
-addEventListener('fetch', createEventHandler({ build, mode: process.env.NODE_ENV }));
+const loadContext = { env: null };
+const handleRemixRequest = createRequestHandler({
+	build,
+	mode: process.env.NODE_ENV,
+	getLoadContext: () => loadContext,
+});
+
+export default {
+	async fetch(request, env, context) {
+		loadContext.env = env; // env vars only available in handler, whereas remix expects the loadContext earlier
+
+		const event = {
+			request,
+			waitUntil(promise) {
+				return ctx.waitUntil(promise);
+			},
+		};
+
+		let response = await handleAsset(event, build, {
+			ASSET_NAMESPACE: env.__STATIC_CONTENT,
+			ASSET_MANIFEST: env.__STATIC_CONTENT_MANIFEST,
+		});
+
+		if (!response) {
+			response = await handleRemixRequest(event);
+		}
+
+		return response;
+	},
+};
